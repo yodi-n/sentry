d88a9508fb7e3a93e9e399a1f791883470487663
Ayesha <ayesha.omarali@gmail.com>
2018-09-12 15:12:30 -0700

    removed containLabel bc it cuts off graphs randomly

--- src/sentry/static/sentry/app/components/charts/components/grid.jsx
+++ src/sentry/static/sentry/app/components/charts/components/grid.jsx
@@ -5,12 +5,11 @@
  */
 export default function Grid(props = {}) {
   return {
-    top: 30,
-    bottom: 20,
+    top: 40,
+    bottom: 40,
     // This should allow for sufficient space for Y-axis labels
     left: '3%',
     right: '3%',
-    containLabel: true,
     ...props,
   };
 }
